ca45a9668ef6c8c80ad803cd0b8540d4926c13de
dyiaskouris <damianosyiaskouris@gmail.com>
2023-12-07 14:21:08 +0000

    game

--- Group18_Game/Assets/SciFi Warehouse Kit/Demo/Scripts/LightFlicker.cs
+++ Group18_Game/Assets/SciFi Warehouse Kit/Demo/Scripts/LightFlicker.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿ using UnityEngine;
  using System.Collections;
  
@@ -41,47 +40,3 @@
 
 
 
-=======
-﻿ using UnityEngine;
- using System.Collections;
- 
- public class LightFlicker : MonoBehaviour
- {
-     public float maximumDim;
-     public float maximumBoost;
-     public float speed;
-     public float strength;
-
-     private bool noFlicker;
-     private Light source;
-     private float initialIntensity;
- 
-     public void Reset()
-     {
-         maximumDim = 0.2f;
-         maximumBoost = 0.2f;
-         speed = 0.1f;
-         strength = 250;
-     }
- 
-     public void Start()
-     {
-         source = GetComponent<Light>();
-         initialIntensity = source.intensity;
-         StartCoroutine(Flicker());
-     }
- 
- 
-     private IEnumerator Flicker()
-     {
-         while (!noFlicker)
-         {
-             source.intensity = Mathf.Lerp(source.intensity, Random.Range(initialIntensity - maximumDim, initialIntensity + maximumBoost), strength * Time.deltaTime);
-             yield return new WaitForSeconds(speed);
-         }
-     }
- }
-
-
-
->>>>>>> Stashed changes
--- Group18_Game/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayRandomSound.cs
+++ Group18_Game/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayRandomSound.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿ using UnityEngine;
  using System.Collections;
  
@@ -27,34 +26,4 @@
          randomSound.Play ();
          StartAudio ();
      }
-=======
-﻿ using UnityEngine;
- using System.Collections;
- 
- public class PlayRandomSound : MonoBehaviour {
- 
-     public AudioSource randomSound;
-     public AudioClip[] audioSources;
-
-     public int clipDelay = 5;
- 
-     // Use this for initialization
-     void Start () {
- 
-         StartAudio ();
-     }
- 
- 
-     void StartAudio()
-     {
-         Invoke ("RandomSoundness", clipDelay);
-     }
- 
-     void RandomSoundness()
-     {
-         randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
-         randomSound.Play ();
-         StartAudio ();
-     }
->>>>>>> Stashed changes
  }
